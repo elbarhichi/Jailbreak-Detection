f91a6686efbe51330ed6c6b133debed2bb0949e5
unknown <yousra.yakhou@centrale-casablanca.ma>
2025-04-06 20:30:14 +0200

    cleaned code

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -5,14 +5,11 @@ import pandas as pd
 import os
 import uuid
 
-# URL de votre gateway Flask
 url = os.getenv("GATEWAY_URL", "http://localhost:5000/send")
 
-# Lecture du fichier CSV et extraction des messages (colonne "prompt")
 df = pd.read_csv('test.csv')
 messages = df['prompt'].tolist()
 
-# Envoi des messages avec leur index en tant qu'ID
 for msg in messages:
     unique_id = str(uuid.uuid4())
     payload = {"id": unique_id, "message": msg}
